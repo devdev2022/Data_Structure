943711801b32b215cbbf8f6daa584aece22b5dd1
khs <devdev0226@gmail.com>
2025-08-05 21:49:14 +0900

    [알고리즘] - 3. 동적 프로그래밍 : 타뷸레이션

--- dev/fibonacci.mjs
+++ dev/fibonacci.mjs
@@ -13,11 +13,30 @@ function fibonacci2(n, memo) {
 }
 
 let start = new Date();
-console.log(fibonacci1(5));
+console.log(fibonacci1(40));
 let end = new Date();
 console.log(`fibonacci1 함수 실행시간 : ${end - start}ms`);
 
 start = new Date();
-console.log(fibonacci2(5, {}));
+console.log(fibonacci2(40, {}));
 end = new Date();
 console.log(`fibonacci2 함수 실행시간 : ${end - start}ms`);
+
+start = new Date();
+console.log(fibonacci3(40));
+end = new Date();
+console.log(`fibonacci3 함수 실행시간 : ${end - start}ms`);
+
+function fibonacci3(n) {
+  if (n === 0 || n === 1) {
+    return n;
+  }
+
+  let table = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    table[i] = table[i - 2] + table[i - 1];
+  }
+
+  return table[n];
+}
